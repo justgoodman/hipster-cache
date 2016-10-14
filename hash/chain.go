8c575679f83970dd6f6e2084cbe76dcc6383db30
EvgenyPak <evgeny.pak@lazada.com>
2016-10-15 02:57:50 +0300

    add work with hash table

---
+++ hash/chain.go
@@ -0,0 +1,55 @@
+package hash
+
+type Chain struct {
+        firstElement *ChainElement
+        lastElement *ChainElement
+        countElements int
+}
+
+type ChainElement struct {
+        key string
+        byteSize int
+        value interface
+}
+
+
+func NewChain(firstElement *ChainElement) &Chain {
+        return &Chain{firstElement: firstElement, lastElement: firstElement, countElements: 1}
+}
+
+
+func NewChainElement(key string, value interface) *Chain {
+        chain := &Chain(key: key)
+        chain.setValue(value)
+        return chain
+}
+
+func (this *ChainElement) getNewByteSize(value interface) int {
+        byteSize := unsage.Sizeof(this) + len(key)
+        switch v := value.(type) {
+                case string:
+                        byteSize += unsafe.Sizeof(this) + len(v)
+        }
+}
+
+func (this *ChainElement) setValue(value interface) int {
+        newByteSize := getNewByteSize(value)
+        deltaByteSize := newByteSize - this.byteSize
+        this.byteSize = getNewByteSize(value)
+        this.value = value
+        return deltaByteSize
+}
+
+func (this *Chain) findElement(key string) *ChainElement {
+        for element := this.firstElement; element != nil; element = element.next {
+                if element.key == key {
+                        return element
+                }
+        }
+}
+
+func (this *Chain) addElement(element *ChainElement) {
+        this.lastElement.next = element
+        this.lastElement = element
+        this.lenght += 1
+}
---
+++ hash/hash_tablel.go
@@ -0,0 +1,149 @@
+package hash
+
+import (
+	"sync"
+	"sync/atomic"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type HashTable struct {
+	capacity int64
+	countElements uint64
+        bytesSize uint64
+	maxChainLenght int
+	chains []*Chain
+	hashFunction *ComplexStringHash
+	mutexChains *sync.RWMutex
+	chainsMutex []*sync.Mutex
+	mutexChainsMutex *sync.RWMutex
+
+	countElementsMetric *prometheus.Gauge
+	bytesSizeMetric *prometheus.Gauge
+        maxChainLenghtMetic *prometheus.Gauge
+	responseTimeMetric *prometheus.SummaryVec
+}
+
+const (
+	namespace = "hispter_cache"
+)
+
+func NewHashTable() *HashTable {
+	return &HashTable{}
+}
+
+func (this *HashTable) initMerics() {
+	this.countElementsMetric := prometheus.NewGauge(prometheus.GaugeOpts{
+			Name:      "elements_total",
+			Help:      "Count of elements stored in hipster cache",
+			Namespace: namespace,
+		})
+
+
+	this.bytesSizeMetric := prometheus.NewGauge(prometheus.GaugeOpts{
+                        Name:      "bytes_total",
+                        Help:      "Size of cache in bytes",
+                        Namespace: namespace,
+                })
+
+	this.maxCheinLenghtMetric := prometheus.NewGauge(prometheus.GaugeOpts{
+                        Name:      "max_chain_lenght_total",
+                        Help:      "Maximum size of chein",
+                        Namespace: namespace,
+                })
+
+	this.responseTimeMetic = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+			Name:      "response_time_microseconds",
+			Help:      "Cache reponse time",
+			Namespace: NAMESPACE,
+		}, []string{"operation"})
+}
+
+//  get duration in microsecond for sub: time1-time2
+func getDurationMicroseconds(duration time.Duration) float64 {
+	return float64(duration) / float64(time.Microsecond)
+}
+
+func (this *HashTable) SetElement(key string,value interface) {
+	timeStart := time.Now()
+
+	hasAdded, chainLenght, deltaBytes := this.setElement(key,value)
+	if hadAdded {
+		countElements := atomic.AddUint64(&this.countElements,1)
+		this.countElementsMetric.Set(float64(countElemenets))
+	}
+	bytesSize := atomic.AddUint64(&this.bytesSize,deltaBytes)
+	this.bytesSizeMetric.Set(float64(butesSize))
+	maxChainLenght = atomic.LoadUint64(&this.maxChainLenght)
+	if maxChainLenght < chainLenght {
+		atomic.StoreUint64(*this.maxChainLenght, chainLenght)
+		this.maxChainLenghtMetric.Set(float64(chainLenght))
+	}
+
+	responseMetric := this.responseTimeMetric.WithLabelValues("set")
+	responseMetric.Observe(getDurationMicroseconds(time.Since(timeStart))
+}
+
+// Return delta byte size
+func (this *HashTable) setElement(key string, value interface) (hasAdded bool, chainLenght int, deltaBytes int) {
+       var (
+		chain *Chain
+		chainMutex *sync.RWMutex
+		ok	bool
+	)
+	hashKey := hashFunction.CalculateHash(key)
+
+	this.mutexChain.RLock()
+	chain, ok := this.chains[hashKey]
+	this.mutexChain.RULock()
+
+	if !ok {
+		chainElement := NewChainElement(key,value)
+		chain := NewChain(chainElement)
+
+		deltaBytes = chainElement.byteSize + unsafe.Sizeof(chain)
+		hasAdded = true
+		chainLeight = 1
+
+		this.mutexChain.Lock()
+		this.chains[hashKey] := chain
+		this.mutexChain.Unlock()
+		return
+	}
+
+	this.mutexChainMutexes.RLock()
+	chainMutex,ok := this.chainsMutex[hashKey]
+	this.mutexChainMutexed.RULock()
+
+	if !ok {
+		chainMutex := &sync.Mutex{}
+		this.mutexChainMutexes.Lock()
+		this.chainsMutex[hasKey] = chainMutex
+		this.mutexChainMutexes.Unlock()
+	}
+
+	chainMutex.Lock()
+
+	element := chain.findElement(key)
+
+	if element != nil {
+		deltaBytes = element.setValue(value)
+		hasAdded = false
+	} else {
+		chainElement := NewChainElement(key,value)
+		chain.AddElement(chainElement)
+
+		deltaBytes = chainElement.sizeButes
+		hasAdded = false
+	}
+
+	chainLenght = chain.lenght
+
+	chainMutex.Unlock()
+
+	return
+}
+
+
+
+
