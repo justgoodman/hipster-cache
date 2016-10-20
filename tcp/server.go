a36ee5380a51f0fa23644126f34659dbf8095cbd
EvgenyPak <evgeny.pak@lazada.com>
2016-10-21 02:25:01 +0300

    add parser for commands

--- tcp/server.go
+++ tcp/server.go
@@ -16,6 +16,11 @@ type CacheServer struct {
 	hashTable *hash_table.HashTable
 }
 
+type ClientMessage struct {
+	command string
+	params  []string
+}
+
 func NewCacheServer(hashTable *hash_table.HashTable, logger common.ILogger, port int) *CacheServer {
 	return &CacheServer{port: port, logger: logger, hashTable: hashTable}
 }
@@ -54,11 +59,83 @@ func (s *CacheServer) handleMessage(conn net.Conn) {
 			}
 			return
 		}
-		fmt.Println(string(buf[0:n]))
-		conn.Write([]byte(string(buf[0:n]) + " Bratish vse ok"))
+		// Remove last symbol => "\n"
+		command := string(buf[0:n])
+		// SET mykey "Hello sdf sdf d df df"
+		// SET
+		// get and replace
+		// split раз и split
+		// GET nonexisting
+		command = command[0 : len(command)-1]
+		fmt.Printf(`Response "%s"`, command)
+		response := s.getResponse(command)
+		conn.Write([]byte(response + "\n"))
 		//	time.Sleep(time.Second * 10)
 		//		conn.Close()
 		//		return
 	}
 	return
+}
+
+func (s *CacheServer) getResponse(command string) string {
+	clientMessage := NewClientMessage()
+	err := clientMessage.Init(command)
+	if err != nil {
+		return err.Error()
+	}
+	return "No error"
+}
+
+func NewClientMessage() *ClientMessage {
+	return &ClientMessage{}
+}
+
+func (m *ClientMessage) Init(value string) error {
+	words := m.splitMessageBySpaceAndQuates(value)
+	fmt.Printf(`\n Words :"%#v"`, words)
+	if len(words) == 0 {
+		return fmt.Errorf(`Error: you don't set the command`)
+	}
+	if len(words) < 2 {
+		return fmt.Errorf(`You don't set any parameters`)
+	}
+	m.command = words[0]
+	m.params = words[1:]
+	return nil
+}
+
+func (m *ClientMessage) splitMessageBySpaceAndQuates(message string) []string {
+	words := []string{}
+	var word string
+	var character string
+	delimeter := ""
+	for _, characterCode := range message {
+		character = string(characterCode)
+		switch character {
+		case ` `:
+			if delimeter == "" {
+				words = append(words, word)
+				word = ""
+				break
+			}
+			word += character
+		case `"`:
+			if delimeter == character {
+				delimeter = ""
+				break
+			}
+			if delimeter == "" {
+				delimeter = character
+				break
+			}
+		case "\n", "\r":
+		default:
+			word += character
+
+		}
+	}
+	if word != "" {
+		words = append(words, word)
+	}
+	return words
 }
