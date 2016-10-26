5b14925f897f27f34c3580c6e94048f09f8e88db
EvgenyPak <evgeny.pak@lazada.com>
2016-10-26 09:14:11 +0300

    Comlete bytesSize in doctionary

--- hash_table/value_type/chain.go
+++ hash_table/value_type/chain.go
@@ -5,49 +5,57 @@ import (
 )
 
 type Chain struct {
-        firstElement *ChainElement
-        lastElement *ChainElement
-        countElements int
-        bytesSize int
+	firstElement  *ChainElement
+	lastElement   *ChainElement
+	countElements int
+	bytesSize     int
 }
 
 type ChainElement struct {
-        bytesSize int
-        value string
-	key string
-	next *ChainElement
+	bytesSize int
+	value     string
+	key       string
+	next      *ChainElement
+}
+
+func (e *ChainElement) updateValue(value string) {
+	lastString := e.value
+	e.value = value
+	e.bytesSize += len(lastString) - len(value)
 }
 
 func NewChain(firstElement *ChainElement) *Chain {
-        chain := &Chain{firstElement: firstElement, lastElement: firstElement, countElements: 1}
+	chain := &Chain{firstElement: firstElement, lastElement: firstElement, countElements: 1}
+	chain.bytesSize = int(unsafe.Sizeof(chain)) + firstElement.bytesSize
 	return chain
 }
 
-func NewChainElement(key,value string) *ChainElement {
-        chainElement := &ChainElement{key:key, value: value}
-        chainElement.bytesSize = int(unsafe.Sizeof(chainElement)) + len(value)
-        return chainElement
+func NewChainElement(key, value string) *ChainElement {
+	chainElement := &ChainElement{key: key, value: value}
+	chainElement.bytesSize = int(unsafe.Sizeof(chainElement)) + len(value)
+	return chainElement
 }
 
-func (this *Chain) findElement(key string) *ChainElement {
-        for element := this.firstElement; element != nil; element = element.next {
-                if element.key == key {
-                        return element
-                }
-        }
+func (c *Chain) findElement(key string) *ChainElement {
+	for element := c.firstElement; element != nil; element = element.next {
+		if element.key == key {
+			return element
+		}
+	}
 	return nil
 }
 
-func (this *Chain) getAllValues() []*DictElement {
-        values := []*DictElement{}
-        for element := this.firstElement; element != nil; element = element.next {
-                  values = append(values,NewDictElement(element.key, element.value))
-        }
-        return values
+func (c *Chain) getAllValues() []*DictElement {
+	values := []*DictElement{}
+	for element := c.firstElement; element != nil; element = element.next {
+		values = append(values, NewDictElement(element.key, element.value))
+	}
+	return values
 }
 
-func (this *Chain) addElement(element *ChainElement) {
-        this.lastElement.next = element
-        this.lastElement = element
-        this.countElements += 1
+func (c *Chain) addElement(element *ChainElement) {
+	c.lastElement.next = element
+	c.lastElement = element
+	c.countElements += 1
+	c.bytesSize += element.bytesSize
 }
--- hash_table/value_type/type_dictionary.go
+++ hash_table/value_type/type_dictionary.go
@@ -2,14 +2,14 @@ package value_type
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 const (
-	SetDictCmdName = "DSET"
-	GetDictCmdName= "DGET"
+	SetDictCmdName    = "DSET"
+	GetDictCmdName    = "DGET"
 	GetAllDictCmdName = "DGETALL"
 )
+
 type SetDictOperation struct {
 	baseOperation
 	key string
@@ -19,54 +19,56 @@ func NewSetDictOperation(key string) *SetDictOperation {
 	return &SetDictOperation{baseOperation: baseOperation{commandName: SetDictCmdName}, key: key}
 }
 
-func (l *SetDictOperation) SetValue(sourceValue *interface{}, value interface{}) (valueSizeBytes int) {
+func (l *SetDictOperation) SetValue(sourceValue *interface{}, value interface{}) (valueBytesSize int) {
 	var stringValue string
-	switch setValue :=  value.(type) {
-		case string:
-			stringValue = setValue
-		default:
-			l.err = fmt.Errorf("Error: list value in not the string")
-			return
+	switch setValue := value.(type) {
+	case string:
+		stringValue = setValue
+	default:
+		l.err = fmt.Errorf("Error: list value in not the string")
+		return
 	}
 
 	switch chain := (*sourceValue).(type) {
-		case *Chain:
-			element := chain.findElement(l.key)
-			if element != nil {
-				element.value = stringValue
-				break
-			}
+	case *Chain:
+		element := chain.findElement(l.key)
+		if element != nil {
+			lastBytesSize := element.bytesSize
+			element.updateValue(stringValue)
+			chain.bytesSize += element.bytesSize - lastBytesSize
+			valueBytesSize = chain.bytesSize
+			break
+		}
 
-			element = NewChainElement(l.key, stringValue)
-			chain.addElement(element)
-			chain.bytesSize += element.bytesSize
+		element = NewChainElement(l.key, stringValue)
+		chain.addElement(element)
 
-			valueSizeBytes = chain.bytesSize
-		// It is like nil, default value
-		case string:
-			if chain != "" {
-				l.err = fmt.Errorf("Error: list type is string")
-			}
-			element := NewChainElement(l.key, stringValue)
-			newChain := NewChain(element)
-			*sourceValue = interface{}(newChain)
+		valueBytesSize = chain.bytesSize
+	// It is like nil, default value
+	case string:
+		if chain != "" {
+			l.err = fmt.Errorf("Error: list type is string")
+		}
+		element := NewChainElement(l.key, stringValue)
+		newChain := NewChain(element)
+		*sourceValue = interface{}(newChain)
 
-			valueSizeBytes = int(unsafe.Sizeof(chain)) + element.bytesSize
-		default:
-			l.err = fmt.Errorf("Error: list type is not the Chain")
+		valueBytesSize = newChain.bytesSize
+	default:
+		l.err = fmt.Errorf("Error: list type is not the Chain")
 	}
 	return
 }
 
-func (o *SetDictOperation) GetResult() (string,error) {
+func (o *SetDictOperation) GetResult() (string, error) {
 	if o.err == nil {
-		return "OK",o.err }
-	return "",o.err
+		return "OK", o.err
+	}
+	return "", o.err
 }
 
-
 type DictElement struct {
-	key string
+	key   string
 	value string
 }
 
@@ -85,24 +87,24 @@ func NewGetAllDictOperation() *GetAllDictOperation {
 
 func (l *GetAllDictOperation) GetValue(value interface{}) {
 	switch chain := value.(type) {
-		case *Chain:
-			l.Elements = chain.getAllValues()
-		default:
-			l.err = fmt.Errorf("Error: list type in not the Chain")
+	case *Chain:
+		l.Elements = chain.getAllValues()
+	default:
+		l.err = fmt.Errorf("Error: list type in not the Chain")
 	}
 }
 
-func (l *GetAllDictOperation) GetResult() (string,error) {
+func (l *GetAllDictOperation) GetResult() (string, error) {
 	result := ""
 	isFirst := true
 	for _, element := range l.Elements {
 		if isFirst {
 			isFirst = false
 		} else {
-		result += "\n"
+			result += "\n"
 		}
-		result +=  fmt.Sprintf(`"%s"`,element.key)
-		result +=  "\n" + fmt.Sprintf(`"%s"`,element.value)
+		result += fmt.Sprintf(`"%s"`, element.key)
+		result += "\n" + fmt.Sprintf(`"%s"`, element.value)
 	}
 	if len(l.Elements) == 0 {
 		result = "(empty dictionary)"
@@ -112,30 +114,30 @@ func (l *GetAllDictOperation) GetResult() (string,error) {
 
 type GetDictOperation struct {
 	baseOperation
-	key string
+	key   string
 	value string
 }
 
 func NewGetDictOperation(key string) *GetDictOperation {
-	return &GetDictOperation{baseOperation: baseOperation{commandName: SetDictCmdName}, key: key,}
+	return &GetDictOperation{baseOperation: baseOperation{commandName: SetDictCmdName}, key: key}
 }
 
 func (l *GetDictOperation) GetValue(value interface{}) {
 
 	switch chain := value.(type) {
-		case *Chain:
-			element := chain.findElement(l.key)
-			if element == nil {
-				l.value = "(nil)"
-			        return
-			}
-			l.value = fmt.Sprintf(`"%s"`,element.value)
-		default:
-			l.err = fmt.Errorf("Error: dict type in not the Chain")
+	case *Chain:
+		element := chain.findElement(l.key)
+		if element == nil {
+			l.value = "(nil)"
+			return
+		}
+		l.value = fmt.Sprintf(`"%s"`, element.value)
+	default:
+		l.err = fmt.Errorf("Error: dict type in not the Chain")
 	}
 	return
 }
 
-func (l *GetDictOperation) GetResult() (string,error) {
-	return l.value,l.err
+func (l *GetDictOperation) GetResult() (string, error) {
+	return l.value, l.err
 }
